f43b27b9fa1f9f7b8f2c791bdddd74abda42f736
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-27 14:32:25 +0300

    converting ispostive normal function to arrow function

---
+++ project 1 - Copy (12)/isPostive.js
@@ -0,0 +1,9 @@
+// normal function
+function isPositive(number) {
+  console.log(number >= 0);
+}
+isPositive();
+
+// arrow function
+let isPostive = (number) => console.log(number >= 0);
+isPostive();
