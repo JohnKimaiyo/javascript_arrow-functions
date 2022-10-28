a5599ed7bcd13fda776a5d1329217c254af62151
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-28 09:42:04 +0300

    converting normal functions to arrow functions

---
+++ project 1 - Copy (10)/isPositive.js
@@ -0,0 +1,9 @@
+// normal function
+function isPostive(number) {
+  console.log(number >= 0);
+}
+isPositive();
+
+// arrow function
+let isPositive = (number) => console.log(number >= 0);
+isPositive();
