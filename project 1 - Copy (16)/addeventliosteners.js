c10f0d38c47947e40297685bd36ae5c6fd591b50
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-28 15:32:39 +0300

    converting normal function to arrow function

---
+++ project 1 - Copy (16)/addeventliosteners.js
@@ -0,0 +1,7 @@
+// normal function
+document.addEventListener("click", function () {
+  console.log("click");
+});
+
+// arrow functuion
+document.addEventListener("click", () => console.log("click"));
---
+++ project 1 - Copy (16)/randomnumber.js
@@ -0,0 +1,8 @@
+// normal function
+function randomNumber() {
+  return Math.random;
+}
+
+// arow function
+let randomNumber2 = () => console.log(Math.random);
+randomNumber2();
