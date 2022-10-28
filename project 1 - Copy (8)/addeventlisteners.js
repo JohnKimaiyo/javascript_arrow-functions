7c5979e3da68ac37f367f82702e337bd386031d4
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-28 12:24:22 +0300

    converting normal function to arrow function

---
+++ project 1 - Copy (8)/addeventlisteners.js
@@ -0,0 +1,7 @@
+// normal function
+document.addEventLsitener("click", function () {
+  console.log("click");
+});
+
+// arrow fucntion
+document.addEventListener("click", () => console.log("click"));
